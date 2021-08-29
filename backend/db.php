79c6c5e99e138fd0e9a47f9e8ab97439bd31515c
Gaurav <gaurav.jss.027@gmail.com>
2021-08-29 12:41:24 +0530

    Database

--- backend/db.php
+++ backend/db.php
@@ -1,5 +1,6 @@
 <?php
-    $con = mysqli_connect("localhost","root","","cra");
+    // $con = mysqli_connect("localhost","root","","cra");
+    $con = mysqli_connect("remotemysql.com","o5aX9bf0BE","ZulxjdzDZv","o5aX9bf0BE");
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
