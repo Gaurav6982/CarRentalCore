8b60ddfd6f41b0530735071c08a5708681e42292
Gaurav <gaurav.jss.027@gmail.com>
2021-09-07 19:58:56 +0530

    Fixed db

--- backend/db.php
+++ backend/db.php
@@ -1,6 +1,7 @@
 <?php
     // $con = mysqli_connect("localhost","root","","cra");
-    $con = mysqli_connect("remotemysql.com","o5aX9bf0BE","ZulxjdzDZv","o5aX9bf0BE");
+    // $con = mysqli_connect("remotemysql.com","o5aX9bf0BE","ZulxjdzDZv","o5aX9bf0BE");
+    $con = mysqli_connect("sql5.freemysqlhosting.net","sql5435327","pYbt5HUuVi","sql5435327");
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
